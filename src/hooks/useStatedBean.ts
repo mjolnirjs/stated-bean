b31995724b4de013564da3693c5e68f95eb89730
JeffJiang <for-eleven@hotmail.com>
2019-08-09 13:47:20 +0800

    useStatedBean hook

---
+++ src/hooks/useStatedBean.ts
@@ -0,0 +1,36 @@
+import { ClassType } from '../types/ClassType';
+import { getStatedBeanContext } from '../context';
+import { useContext, useState, useEffect } from 'react';
+
+export function useStatedBean<T>(
+  type: ClassType<T>,
+  dependentFields?: Array<string | symbol>
+): T {
+  const StateBeanContext = getStatedBeanContext();
+  const context = useContext(StateBeanContext);
+
+  if (context.container === undefined) {
+    throw new Error('not found container');
+  }
+  const bean = context.container.getBean(type) as any;
+  const change_event = Symbol.for(bean.constructor.name + '_change');
+  const [version, setVersion] = useState(0);
+
+  const beanChangeListener = (_bean: T, field: string) => {
+    if (dependentFields === undefined) {
+      setVersion(prev => prev + 1);
+    } else if (dependentFields.includes(field)) {
+      setVersion(prev => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    context.container!.on(change_event, beanChangeListener);
+
+    return () => {
+      context.container!.off(change_event, beanChangeListener);
+    };
+  }, []);
+
+  return bean;
+}

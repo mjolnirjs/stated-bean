fb678631a01f97f4371599c6c36914c948c4a073
JeffJiang <for-eleven@hotmail.com>
2019-08-09 13:50:26 +0800

    export all

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
-
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+export * from './container';
+export * from './decorator';
+export * from './hooks';
+export * from './context';

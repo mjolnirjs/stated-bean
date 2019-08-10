b45db16ca19c7866a02694bca5f185aa9a2ea6f6
foreleven <for-eleven@hotmail.com>
2019-08-10 19:47:19 +0800

    fix(event): emit function parameter type

--- src/event/Event.ts
+++ src/event/Event.ts
@@ -1,20 +1,21 @@
-type EventListenFn = (...args: any) => void
-type EventTypes = Map<string | symbol, EventListenFn[]>
+type EventListenFn = (...args: any) => void;
+type EventTypes = Map<string | symbol, EventListenFn[]>;
+
 export class Event {
-    events: EventTypes = new Map()
-    on(evtName: string | symbol, cb: EventListenFn) {
-        this.events.set(evtName, (this.events.get(evtName) || []).concat(cb))
+  events: EventTypes = new Map();
+  on(evtName: string | symbol, cb: EventListenFn) {
+    this.events.set(evtName, (this.events.get(evtName) || []).concat(cb));
+  }
+  emit(evtName: string | symbol, ...data: any) {
+    if (this.events.has(evtName)) {
+      this.events.get(evtName)!.forEach(cb => {
+        cb(...data);
+      });
     }
-    emit(evtName: string | symbol, data: any) {
-        if (this.events.has(evtName)) {
-            this.events.get(evtName)!.forEach(cb => {
-                cb(data)
-            });
-        }
+  }
+  off(evtName: string | symbol, cb: EventListenFn) {
+    if (this.events.has(evtName)) {
+      this.events.set(evtName, this.events.get(evtName)!.filter(c => c !== cb));
     }
-    off(evtName: string | symbol, cb: EventListenFn) {
-        if (this.events.has(evtName)) {
-            this.events.set(evtName, this.events.get(evtName)!.filter(c => c !== cb))
-        }
-    }
+  }
 }

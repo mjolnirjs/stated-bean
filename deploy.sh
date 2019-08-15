f6f6ebd6eca8ff1c398d74618434b7111e62efb8
JounQin <admin@1stg.me>
2019-08-15 11:45:48 +0800

    ci: deploy npm package automatically on CI

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+git remote set-url origin "https://user:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+npm set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
+
+git fetch origin "$TRAVIS_BRANCH":"$TRAVIS_BRANCH"
+git checkout "$TRAVIS_BRANCH"
+
+cd dist
+
+PKG_VERSION=$(jq -r '.version' package.json)
+
+git fetch origin v"$PKG_VERSION" || {
+  yarn add -D standard-version
+  yarn run standard-version -a --release-as "$PKG_VERSION"
+  git push --follow-tags origin "$TRAVIS_BRANCH"
+  npm publish --access=public
+}

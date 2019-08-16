596b1b63a41082bcba91f3212e453c5eb1a3b82f
JounQin <admin@1stg.me>
2019-08-16 20:56:28 +0800

    fix: workaround for descriptor undefined

--- src/decorator/PostProvided.ts
+++ src/decorator/PostProvided.ts
@@ -3,8 +3,11 @@ import { getMetadataStorage } from '../metadata';
 export const PostProvided = (): MethodDecorator => (
   prototype,
   propertyKey,
-  descriptor: TypedPropertyDescriptor<any>,
+  descriptor?: TypedPropertyDescriptor<any>,
 ) => {
+  if (descriptor === undefined) {
+    descriptor = Object.getOwnPropertyDescriptor(prototype, propertyKey)!;
+  }
   getMetadataStorage().collectPostProvided({
     name: propertyKey,
     target: prototype.constructor,

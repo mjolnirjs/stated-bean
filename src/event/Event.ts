b2f7648088a58025830d79138b13a8c28e4f190a
foreleven <for-eleven@hotmail.com>
2019-08-27 00:03:01 +0800

    fix: use the bean as event key

--- src/event/Event.ts
+++ src/event/Event.ts
@@ -1,14 +1,16 @@
+import { ClassType } from '../types';
+
 type EventListenFn = (...args: any) => void;
-type EventTypes = WeakMap<Function, EventListenFn[]>;
+type EventTypes = WeakMap<InstanceType<ClassType>, EventListenFn[]>;
 
 export class Event {
   events: EventTypes = new WeakMap();
 
-  on(type: Function, cb: EventListenFn) {
+  on(type: InstanceType<ClassType>, cb: EventListenFn) {
     this.events.set(type, (this.events.get(type) || []).concat(cb));
   }
 
-  emit(type: Function, ...data: any) {
+  emit(type: InstanceType<ClassType>, ...data: any) {
     if (this.events.has(type)) {
       this.events.get(type)!.forEach(cb => {
         cb(...data);
@@ -16,7 +18,7 @@ export class Event {
     }
   }
 
-  off(type: Function, cb: EventListenFn) {
+  off(type: InstanceType<ClassType>, cb: EventListenFn) {
     if (this.events.has(type)) {
       this.events.set(type, this.events.get(type)!.filter(c => c !== cb));
     }
